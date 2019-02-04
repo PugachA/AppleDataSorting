7a3a93711a350eb751afb2275f03c6b5d7e0f7cb
Александр Пугач <fotone97@yandex.ru>
2019-02-04 15:52:36 +0300

    Добавьте файлы проекта.

---
+++ AppleDataSorting/MainWindow.xaml.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Path = System.IO.Path;
+using System.Windows.Forms;
+
+namespace AppleDataSorting
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        string sourceDir;
+        string sortedJPGDir;
+        string sortedMOVDir;
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            sourceDir = sourceTextBox.Text;
+            sortedJPGDir = resultTextBox.Text + @"\Pictures";
+            sortedMOVDir = resultTextBox.Text + @"\Movies";
+            InfoTextBlock.Text = "Начали разбирать";
+            Task categoriesTask = Task.Factory.StartNew(Processing);
+
+        }
+
+        private void Processing()
+        {
+            Directory.CreateDirectory(sortedJPGDir);
+            Directory.CreateDirectory(sortedMOVDir);
+            string[] picList = Directory.GetFiles(sourceDir, "*.jpg");
+            string[] movList = Directory.GetFiles(sourceDir, "*.mov");
+            foreach (string f in picList)
+            {
+                // Remove path from the file name.
+                string fName = f.Substring(sourceDir.Length + 1);
+
+                // Use the Path.Combine method to safely append the file name to the path.
+                // Will overwrite if the destination file already exists.
+                File.Copy(Path.Combine(sourceDir, fName), Path.Combine(sortedJPGDir, fName), true);
+            }
+
+            foreach (string f in movList)
+            {
+                // Remove path from the file name.
+                string fName = f.Substring(sourceDir.Length + 1);
+
+                // Use the Path.Combine method to safely append the file name to the path.
+                // Will overwrite if the destination file already exists.
+                File.Copy(Path.Combine(sourceDir, fName), Path.Combine(sortedMOVDir, fName), true);
+            }
+            Dispatcher.Invoke(delegate { InfoTextBlock.Text = "Готово"; });
+        }
+
+        private void sourceDirectoryPath_Click(object sender, RoutedEventArgs e)
+        {
+            //Microsoft.Win32.OpenFileDialog openFileDialog = new Microsoft.Win32.OpenFileDialog();
+            //if (openFileDialog.ShowDialog() == true)
+            //{
+            //    FileInfo fi = new FileInfo(openFileDialog.FileName);
+            //    sourceDir=fi.DirectoryName;
+            //}
+            FolderBrowserDialog folderBrowser = new FolderBrowserDialog();
+            DialogResult result = folderBrowser.ShowDialog();
+            sourceTextBox.Text = folderBrowser.SelectedPath;
+        }
+
+        private void resultDirectoryPath_Click(object sender, RoutedEventArgs e)
+        {
+            FolderBrowserDialog folderBrowser = new FolderBrowserDialog();
+            DialogResult result = folderBrowser.ShowDialog();
+            resultTextBox.Text = folderBrowser.SelectedPath;
+        }
+    }
+}
